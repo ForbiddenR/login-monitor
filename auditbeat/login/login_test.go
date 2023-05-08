525561f51d75ae504edbcc921bb3806c958a0cac
ForbiddenR <hostyouposition@outlook.com>
2023-05-08 14:15:41 +0800

    add some help functions

--- auditbeat/login/login_test.go
+++ auditbeat/login/login_test.go
@@ -2,7 +2,9 @@ package login
 
 import (
 	"encoding/binary"
+	"io"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/elastic/beats/v7/auditbeat/core"
@@ -30,7 +32,7 @@ func TestWtmp(t *testing.T) {
 
 	if len(events) == 0 {
 		t.Fatal("no evnets were generated")
-	}else if len(events) != 1 {
+	} else if len(events) != 1 {
 		t.Fatalf("only one event expected, got %d", len(events))
 	}
 
@@ -39,10 +41,64 @@ func TestWtmp(t *testing.T) {
 	mbtest.WriteEventToDataJSON(t, fullEvent, "")
 }
 
+func TEstWtmp(t *testing.T) {
+	if byteOrder != binary.LittleEndian {
+		t.Skip("Test only works on little-endian system - skipping.")
+	}
+
+	defer SetupDataDir(t)()
+
+}
+
 func getBaseConfig() map[string]interface{} {
 	return map[string]interface{}{
 		"module":   "system",
 		"datasets": []string{"login"},
+	}
+}
+
+// setupTestDir creates a temporary directory, copies the test files into it,
+// and returns the path.
+func setupTestDir(t *testing.T) string {
+	tmp, err := os.MkdirTemp("", "auditbeat-login-test-dir")
+	if err != nil {
+		t.Fatal("failed to create temp dir")
+	}
+
+	copyDir(t, "./testdata", tmp)
+
+	return tmp
+}
+
+func copyDir(t *testing.T, src, dst string) {
+	files, err := os.ReadDir(src)
+	if err != nil {
+		t.Fatalf("failed to read %v", src)
+	}
+
+	for _, file := range files {
+		srcFile := filepath.Join(src, file.Name())
+		dstFile := filepath.Join(dst, file.Name())
+		copyFile(t, srcFile, dstFile)
+	}
+}
+
+func copyFile(t *testing.T, src, dst string) {
+	in, err := os.Open(src)
+	if err != nil {
+		t.Fatalf("failed to open %v", src)
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		t.Fatalf("failed to open %v", dst)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		t.Fatalf("failed to copy %v to %v", src, dst)
 	}
 }
 
